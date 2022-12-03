d3638879b1335ece207179f39e199f3e23a8d362
nikolainc <nikolai9710@gmail.com>
2022-12-03 18:19:34 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_GB_Python_03/HomeWork_GB_Python_03.py
@@ -0,0 +1,11 @@
+﻿# 1 Вводим с клавиатуры десятичное число. Необходимо перевести его в шестнадцатиричную систему счисления.
+
+#listOfHexNumbers = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
+#number = int(input("Введите десятичное число\n"))
+#result = []
+#while number > 0:
+#    result.insert(0, listOfHexNumbers.get(int(number % 16)))
+#    number //= 16
+#result = [str(i) for i in result]
+#print("".join(result))
+
